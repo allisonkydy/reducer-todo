2860d6c9aeb7159695be13370262a1fd8aadcebe
allisonkydy <allisnelly@gmail.com>
2019-09-10 14:23:25 -0500

    built initial state and simple reducer

---
+++ reducer-todo/src/reducers/todoReducer.js
@@ -0,0 +1,24 @@
+
+export const initialState = {
+  todos: [
+    {
+      item: 'Learn about reducers',
+      completed: false,
+      id: 3892987586
+    },
+    {
+      item: 'Eat grass',
+      completed: false,
+      id: 3892987587
+    },
+    {
+      item: 'Feed worms',
+      completed: false,
+      id: 3892987589
+    },
+  ]
+};
+
+export const todoReducer = (state, action) => {
+  return state;
+}
