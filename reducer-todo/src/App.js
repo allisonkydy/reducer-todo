b299378673f72e176eef2a71967d228efa133d1b
allisonkydy <allisnelly@gmail.com>
2019-09-10 14:28:52 -0500

    set up components

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
 function App() {
   return (
     <div className="App">
-      hello
+      <h1>a new and fancy todo app</h1>
+      <TodoForm />
+      <TodoList />
     </div>
   );
 }
