5a01818d1e938dc401837f2aaaaf0015d0a16b0c
allisonkydy <allisnelly@gmail.com>
2019-09-10 14:43:12 -0500

    built out todoform

--- reducer-todo/src/components/TodoForm.js
+++ reducer-todo/src/components/TodoForm.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function TodoForm() {
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // call dispatch fn
+    setNewTodo("");
+  }
+
+  const handleClear = (e) => {
+    e.preventDefault();
+    // call remove fn
+  }
+
   return (
-    <div></div>
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        placeholder="enter a todo..."
+        value={newTodo}
+        name="newTodo"
+        onChange={handleInputChange}
+      />
+      <button type="submit">add</button>
+      <button type="button" onClick={(e) => handleClear(e)}>clear completed</button>
+    </form>
   )
 }
 
