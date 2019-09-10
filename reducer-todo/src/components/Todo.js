4a2d333e999154816d44f8583b5411a47b8fb37c
allisonkydy <allisnelly@gmail.com>
2019-09-10 16:54:34 -0500

    completed todos show date and time of completion

--- reducer-todo/src/components/Todo.js
+++ reducer-todo/src/components/Todo.js
@@ -1,12 +1,14 @@
 import React from "react";
+import moment from 'moment';
 
 function Todo({ todo, toggleComplete }) {
   return (
     <div
       onClick={() => toggleComplete(todo.id)}
-      className={todo.completed ? "complete" : "incomplete"}
+      className={todo.completed ? "todo-item complete" : "todo-item incomplete"}
     >
       {todo.item}
+      {todo.completed && <div className="todo-date">Completed {moment().format('MMM Do YYYY, h:mm a')}</div>}
     </div>
   );
 }
